73d35a3ffce40230709aebd041fa7ee0aafc466f
danuja01 <it21023378@my.sliit.lk>
2023-10-18 09:05:52 +0530

    Initialize fonts and created bottom nav

--- app/home/index.js
+++ app/home/index.js
@@ -1,14 +1,14 @@
 import React from "react";
-import { SafeAreaView, Text, View } from "react-native";
+import { SafeAreaView, ScrollView, Text, View } from "react-native";
 import { Stack } from "expo-router";
 
 export default Home = () => {
   return (
     <SafeAreaView style={{ flex: 1, backgroundColor: "#ffffff" }}>
       <Stack.Screen options={{ header: () => null }} />
-      <View>
+      <ScrollView showsVerticalScrollIndicator={false}>
         <Text>web</Text>
-      </View>
+      </ScrollView>
     </SafeAreaView>
   );
 };
